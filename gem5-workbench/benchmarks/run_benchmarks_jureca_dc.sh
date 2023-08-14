b29cb6f4d93e1f35c460c3f4266669cd43aab969
Stepan Nassyr <s.nassyr@xcpp.org>
2023-08-14 19:49:45 +0200

    Improve a log message

--- gem5-workbench/benchmarks/run_benchmarks_jureca_dc.sh
+++ gem5-workbench/benchmarks/run_benchmarks_jureca_dc.sh
@@ -13,7 +13,7 @@ for vlen_idx in "${!vlens[@]}"; do
         node=${nodes[$node_idx]}
         vlen=${vlens[$vlen_idx]}
         assoc=${assocs[$ass_idx]}
-        echo "Running vlen ${vlen} on node ${node}"
+        echo "Running vlen ${vlen}/assoc ${assoc} on node ${node}"
         srun --exact -n 1 -N 1 --cpu-bind=none -w ${node} bash benchmarks/gemm_gem5_benchmarks.sh ${vlen} ${assoc} &
     done
 done
