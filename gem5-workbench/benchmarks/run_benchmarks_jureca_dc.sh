b520b84a97f7bf7e0eaeb5ccd3d7bbd563a85f5f
Stepan Nassyr <s.nassyr@xcpp.org>
2023-08-14 19:48:29 +0200

    associativity in jureca batch script

--- gem5-workbench/benchmarks/run_benchmarks_jureca_dc.sh
+++ gem5-workbench/benchmarks/run_benchmarks_jureca_dc.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-#SBATCH -N 4
-#SBATCH -n 4
+#SBATCH -N 12
+#SBATCH -n 12
 #SBATCH -A zam
 #SBATCH -p dc-cpu
 
 nodes=($(cluset -e "$SLURM_JOB_NODELIST"))
 vlens=(128 256 512 1024)
-for vlen_idx in "${!vlens[@]}"; do  
-    node=${nodes[$vlen_idx]}
-    vlen=${vlens[$vlen_idx]}
-    echo "Running vlen ${vlen} on node ${node}"
-    srun --exact -n 1 -N 1 --cpu-bind=none -w ${node} bash benchmarks/gemm_gem5_benchmarks.sh ${vlen} &
+assocs=(4 8 16)
+for vlen_idx in "${!vlens[@]}"; do
+    for ass_idx in "${!assocs[@]}"; do
+        node_idx=$((vlen_idx*3+ass_idx))
+        node=${nodes[$node_idx]}
+        vlen=${vlens[$vlen_idx]}
+        assoc=${assocs[$ass_idx]}
+        echo "Running vlen ${vlen} on node ${node}"
+        srun --exact -n 1 -N 1 --cpu-bind=none -w ${node} bash benchmarks/gemm_gem5_benchmarks.sh ${vlen} ${assoc} &
+    done
 done
 
 wait
