708725f7921791fdb6aca4ee835981193af0ad41
Stepan Nassyr <s.nassyr@fz-juelich.de>
2023-08-18 20:51:27 +0200

    create_table: fix edge case and improve latex style

--- gem5-workbench/analysis/create_table.py
+++ gem5-workbench/analysis/create_table.py
@@ -43,14 +43,15 @@ def main():
 
         result = {}
         print(f"Combination ({nfu},{vlen},{lat})")
-        result["(\\NSIMD,\\wSIMD,\\lambdaSIMDfma)"] = f"$({nfu},{vlen},{lat})$"
+        result[r"$(\NSIMD,\wSIMD,\lambdaSIMDfma)$"] = f"$({nfu},{vlen},{lat})$"
 
         df_good = df_efficient[ \
                 (df_efficient["nfu"] == nfu) & \
                 (df_efficient["lat"] == lat) & \
                 (df_efficient["vlen"] == vlen)]
 
-        df_good = df_good.groupby(["mr","nr"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
+        if(0 != len(df_good)):
+            df_good = df_good.groupby(["mr","nr"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
 
         result["\\ngood"] = len(df_good)
 
@@ -63,28 +64,37 @@ def main():
         best_idx = df_good["efficiency"].idxmax()
         best = df_good.loc[best_idx]
 
-        result["\\epsfpmax"] = best["efficiency"]
-        result["(m_r,n_r,k_c)"] = f"$({int(best['mr'])},{int(best['nr'])},{int(best['kc'])})$"
-        result["\\dt [cycle]"] = int(best["system.cpu.numCycles"])
-        result["\\nrarchmin"] = int(best["mr"]*best["nr"]+1+2*best["mr"])
-        result["\\nrphysvmax"] = int(256-(1000-best["system.cpu.rename.max1000MinusVecFreeEntries"]))
-        result["\\nrobmax"] = int(best["system.cpu.rob.maxNumInstsInROB"])
-        result["\\nreservmax"] = int(120- best["system.cpu.numFreeEntriesDist::min_value"])
-        result["\\nbissuemax"] = best["system.cpu.numIssuedDist::max_value"]
-        result["\\IPC"] = f"{best['system.cpu.ipc']:.1f}"
-        result["\\nbimipcrate"] = best["system.cpu.ipc"]/best["system.cpu.numIssuedDist::max_value"]
+        result[r"$\epsfpmax$"] = best["efficiency"]
+        result[r"$(m_r,n_r,k_c)$"] = f"$({int(best['mr'])},{int(best['nr'])},{int(best['kc'])})$"
+        result[r"$\dt$ [cycle]"] = int(best["system.cpu.numCycles"])
+        result[r"$\nrarchmin$"] = int(best["mr"]*best["nr"]+1+2*best["mr"])
+        result[r"$\nrphysvmax$"] = int(256-(1000-best["system.cpu.rename.max1000MinusVecFreeEntries"]))
+        result[r"$\nrobmax$"] = int(best["system.cpu.rob.maxNumInstsInROB"])
+        result[r"$\nreservmax$"] = int(120- best["system.cpu.numFreeEntriesDist::min_value"])
+        #result[r"$\nbissuemax$"] = best["system.cpu.numIssuedDist::max_value"]
+        result[r"$\IPC$"] = f"{best['system.cpu.ipc']:.1f}"
+        #result[r"$\nbimipcrate$"] = best["system.cpu.ipc"]/best["system.cpu.numIssuedDist::max_value"]
 
         results.append(result)
 
     df_results = pandas.DataFrame(results)
 
-    meanrate = df_results["\\nbimipcrate"].mean()
-    maxfactor = 1.0/df_results["\\nbimipcrate"].min()
+    #meanrate = df_results["\\nbimipcrate"].mean()
+    #maxfactor = 1.0/df_results["\\nbimipcrate"].min()
+    #print(f"IPC/issue avg: {meanrate}; up to {maxfactor} more issued than IPC")
 
-    print(f"IPC/issue avg: {meanrate}; up to {maxfactor} more issued than IPC")
-    print(df_results.to_latex(index=False,
-                              escape=False,
-                              float_format="%.3f"))
+    #print(df_results.to_latex(index=False,
+    #                          escape=False,
+    #                          float_format="%.3f"))
+    df_results.set_index(r"$(\NSIMD,\wSIMD,\lambdaSIMDfma)$",inplace=True)
+    #df_results.reset_index(drop=True,inplace=True)
+    print(df_results.style.to_latex(
+        environment="table*",
+        column_format="".join(['c' for c in range(df_results.columns.size)]),
+        position_float="centering",
+        hrules=True,
+        caption="Summary results for each design point. The leftmost columns show the results for the most efficient kernel.",
+        label="tab:simres_overview"))
 
 
 
