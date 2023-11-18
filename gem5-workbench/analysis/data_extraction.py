971ba0bcd737281af67d1759c9ccf92df7b78fc1
Stepan Nassyr <s.nassyr@xcpp.org>
2023-11-18 16:38:48 +0100

    analysis: int. new plot type, separate file for data extraction

---
+++ gem5-workbench/analysis/data_extraction.py
@@ -0,0 +1,90 @@
+
+import pandas
+import os
+import sys
+
+MIN_PYTHON = (3, 9)
+if sys.version_info < MIN_PYTHON:
+    sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
+
+
+index_params = ["mr","nr",
+                "simd_lat","simd_count","simd_width","simd_phreg_count",
+                "ld_count","st_count",
+                "l1_size","assoc",
+                "decode_width","commit_width",
+                "iq_size","rob_size",
+                "fetch_buf_size",
+                "run"]
+
+def extract_target(target_stat:str,
+                   statfile_path:os.PathLike):
+    try:
+        df = pandas.read_hdf(statfile_path, key="gem5stats")
+    except Exception as exc:
+        print(f"error occured: {exc}")
+
+    df["minCyclesPossible"] = (df["system.cpu.commitStats0.committedInstType::SimdFloatMultAcc"] +\
+            df["system.cpu.commitStats0.committedInstType::SimdFloatMult"])/df["simd_count"]
+    df["efficiency"] = df["minCyclesPossible"]/df["system.cpu.numCycles"]
+
+    return df[index_params+[target_stat]]
+
+def build_df(directory: os.PathLike,
+             target_stat: str):
+    import tqdm
+    import multiprocessing as mp
+    from multiprocessing.pool import Pool
+    import psutil
+    import functools
+
+
+    statfile_list = []
+    for root, _, files in os.walk(directory, topdown=False):
+        for name in files:
+            if name.endswith(".h5"):
+                statfile_list.append(os.path.join(root, name))
+
+    statfile_count = len(statfile_list)
+
+    ram_available = psutil.virtual_memory().total
+    hw_cores = int(os.cpu_count())
+    print(f"System has {hw_cores} hardware cores")
+    ram_per_worker = 1000*2**20
+    hw_max_ram_cores = int((0.50*ram_available)/ram_per_worker)
+    print(f"System has enough memory for {hw_max_ram_cores} concurrent workers")
+    hw_cores = min(hw_cores, hw_max_ram_cores)
+    max_workers = min(hw_cores, statfile_count)
+
+    stat_df = pandas.DataFrame()
+    df_list = []
+    df_merge_count = 100
+    with Pool(max_workers) as pool:
+        for result in tqdm.tqdm(
+                pool.imap_unordered(
+                    functools.partial(
+                        extract_target,
+                        target_stat
+                        ),
+                        statfile_list
+                    ),
+                unit='files',
+                desc='Extracting data: ',
+                total=statfile_count,
+                delay=1,
+                smoothing=0.1,
+                ):
+            if stat_df.empty:
+                stat_df = result
+            else:
+                df_list.append(result)
+
+            if len(df_list) > df_merge_count:
+                stat_df = pandas.concat([stat_df]+df_list)
+                df_list = []
+
+    if df_list:
+        stat_df = pandas.concat([stat_df]+df_list)
+        df_list = []
+
+    return stat_df
