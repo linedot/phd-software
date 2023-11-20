706750c1fd6b06b55b8a9acdb9e35160a59207b8
Stepan Nassyr <s.nassyr@fz-juelich.de>
2023-11-20 20:29:48 +0100

    update/change jusuf slurm script

--- gem5-workbench/benchmarks/run_benchmarks_jusuf.sh
+++ gem5-workbench/benchmarks/run_benchmarks_jusuf.sh
@@ -3,19 +3,19 @@
 #SBATCH -n 12
 #SBATCH -A zam
 #SBATCH -p batch
-#SBATCH --time=04:00:00
+#SBATCH --time=06:00:00
 
 nodes=($(scontrol show hostnames "$SLURM_JOB_NODELIST"))
 vlens=(128 256 512 1024)
-assocs=(4 8 16)
+vcnts=(1 2 4)
 for vlen_idx in "${!vlens[@]}"; do
-    for ass_idx in "${!assocs[@]}"; do
-        node_idx=$((vlen_idx*3+ass_idx))
+    for vcnt_idx in "${!vcnts[@]}"; do
+        node_idx=$((vlen_idx*3+vcnt_idx))
         node=${nodes[$node_idx]}
         vlen=${vlens[$vlen_idx]}
-        assoc=${assocs[$ass_idx]}
-        echo "Running vlen ${vlen}/assoc ${assoc} on node ${node}"
-        srun --exact -n 1 -N 1 --cpu-bind=none -w ${node} bash benchmarks/gemm_gem5_benchmarks.sh ${vlen} ${assoc} &
+        vcnt=${vcnts[$vcnt_idx]}
+        echo "Running vlen ${vlen}/vcnt ${vcnt} on node ${node}"
+	PYTHONPATH=bine-configs:${HOME}/.local/lib/python3.11/site-packages:$PYTHONPATH srun --exact -N1 -n1 --cpu-bind=none -w ${node} build/ALL/gem5.opt --no-output-files configs/multi-isa-nanogemm.py --isa aarch64 --mr {1..8} --nr {1..30} --simd_lat 4 --simd_count ${vcnt} --simd_width ${vlen} --decode_width 8 --commit_width 8 --fetch_buf_size 64 --assoc 8 --l1_size 64 --ld_count 2 --st_count 2 --simd_phreg_count 48 52 56 60 64 68 72 76 80 84 88 92 96 104 112 120 128 --rob_size 16 18 20 22 24 26 28 32 36 40 44 48 52 56 60 64 128 --iq_size 8 10 12 14 16 18 20 24 28 32 --split_bytes 10000000000 --base_out_dir=${SCRATCH}/nassyr1/nanogemm-$(date +'%F')-job${SLURM_JOBID} --quiet --stat_filename stats_vlen${vlen}_vcnt${vcnt}_ --tqdm_position ${node_idx} &
     done
 done
 
