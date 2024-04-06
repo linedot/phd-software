b3d6d1cda55a2ab36a67d880cd4d08c464abf392
Stepan Nassyr <s.nassyr@xcpp.org>
2024-04-06 20:07:30 +0200

    specific_kernel: update stat/parameter names

--- gem5-workbench/analysis/specific_kernels.py
+++ gem5-workbench/analysis/specific_kernels.py
@@ -24,60 +24,60 @@ def main():
 
     # Now handled by the extract script
     #df["minCyclesPossible"] = (df["system.cpu.commitStats0.committedInstType::SimdFloatMultAcc"] +\
-    #        df["system.cpu.commitStats0.committedInstType::SimdFloatMult"])/df["nfu"]
+    #        df["system.cpu.commitStats0.committedInstType::SimdFloatMult"])/df["simd_count"]
     #df["efficiency"] = df["minCyclesPossible"]/df["system.cpu.numCycles"]
-    #data_size = 8 # double
-    #df["bytesRead"] = df["mr"]*df["kc"]*(df["vlen"]/8) + df["kc"]*df["nr"]*data_size + df["mr"]*df["nr"]*(df["vlen"]/8)
-    #df["bytesWritten"] = df["mr"]*df["nr"]*(df["vlen"]/8)
+    data_size = 8 # double
+    df["bytesRead"] = df["mr"]*df["kc"]*(df["simd_width"]/8) + df["kc"]*df["nr"]*data_size + df["mr"]*df["nr"]*(df["simd_width"]/8)
+    df["bytesWritten"] = df["mr"]*df["nr"]*(df["simd_width"]/8)
 
-    df_nfu1_lat4 = df[(df["nfu"] == 1) & (df["lat"] == 4)]
-    df_nfu2_lat10 = df[(df["nfu"] == 2) & (df["lat"] == 10)]
-    df_nfu2_lat4 = df[(df["nfu"] == 2) & (df["lat"] == 4)]
+    df_simd_count1_lat4 = df[(df["simd_count"] == 1) & (df["simd_lat"] == 4)]
+    df_simd_count2_lat10 = df[(df["simd_count"] == 2) & (df["simd_lat"] == 10)]
+    df_simd_count2_lat4 = df[(df["simd_count"] == 2) & (df["simd_lat"] == 4)]
 
-    df_assoc4 = df_nfu1_lat4[df_nfu1_lat4["assoc"] == 4]
+    df_assoc4 = df_simd_count1_lat4[df_simd_count1_lat4["assoc"] == 4]
 
-    df_good = df_assoc4.groupby(["mr","nr","vlen"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
+    df_good = df_assoc4.groupby(["mr","nr","simd_width"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
     df_good.reset_index(drop = True, inplace = True)
 
     df_good["l1bw"] = (df_good["bytesRead"]+df_good["bytesWritten"])/df_good["system.cpu.numCycles"]
 
-    df_mr2_nr4_vlen128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["vlen"] == 128)]
+    df_mr2_nr4_simd_width128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["simd_width"] == 128)]
 
-    print(df_mr2_nr4_vlen128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr4_simd_width128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
-    df_mr2_nr1_vlen128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 1)& (df_good["vlen"] == 128)]
+    df_mr2_nr1_simd_width128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 1)& (df_good["simd_width"] == 128)]
 
-    print(df_mr2_nr1_vlen128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr1_simd_width128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
-    df_assoc4 = df_nfu2_lat10[df_nfu2_lat10["assoc"] == 4]
+    df_assoc4 = df_simd_count2_lat10[df_simd_count2_lat10["assoc"] == 4]
 
-    df_good = df_assoc4.groupby(["mr","nr","vlen"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
+    df_good = df_assoc4.groupby(["mr","nr","simd_width"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
     df_good.reset_index(drop = True, inplace = True)
 
     df_good["l1bw"] = (df_good["bytesRead"]+df_good["bytesWritten"])/df_good["system.cpu.numCycles"]
 
-    df_mr2_nr10_vlen128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 10)& (df_good["vlen"] == 512)]
+    df_mr2_nr10_simd_width128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 10)& (df_good["simd_width"] == 512)]
 
-    print(df_mr2_nr10_vlen128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr10_simd_width128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
-    df_mr2_nr4_vlen128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["vlen"] == 512)]
+    df_mr2_nr4_simd_width128 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["simd_width"] == 512)]
 
-    print(df_mr2_nr4_vlen128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr4_simd_width128[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
-    df_assoc4 = df_nfu2_lat4[df_nfu2_lat4["assoc"] == 4]
+    df_assoc4 = df_simd_count2_lat4[df_simd_count2_lat4["assoc"] == 4]
 
-    df_good = df_assoc4.groupby(["mr","nr","vlen"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
+    df_good = df_assoc4.groupby(["mr","nr","simd_width"], group_keys=False).apply(lambda x: x.loc[x.efficiency.idxmax()])
     df_good.reset_index(drop = True, inplace = True)
 
     df_good["l1bw"] = (df_good["bytesRead"]+df_good["bytesWritten"])/df_good["system.cpu.numCycles"]
 
-    df_mr2_nr4_vlen256 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["vlen"] == 256)]
+    df_mr2_nr4_simd_width256 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 4)& (df_good["simd_width"] == 256)]
 
-    print(df_mr2_nr4_vlen256[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr4_simd_width256[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
-    df_mr2_nr2_vlen256 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 2)& (df_good["vlen"] == 256)]
+    df_mr2_nr2_simd_width256 = df_good[(df_good["mr"] == 2) & (df_good["nr"] == 2)& (df_good["simd_width"] == 256)]
 
-    print(df_mr2_nr2_vlen256[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
+    print(df_mr2_nr2_simd_width256[["mr","nr","kc","unroll","flops","minCyclesPossible","efficiency","bytesRead","bytesWritten","l1bw"]])
 
 
 
